bc668ac3325503a1e175cf11b5a332d67c954621
Lee Wise <theleewise@users.noreply.github.com>
2024-11-25 14:34:30 -0600

    add year argument to tests

--- src/tests/2024.test.ts
+++ src/tests/2024.test.ts
@@ -7,13 +7,13 @@ describe('2024 Rates', () => {
 
 describe('2024 Additional Payments', () => {
   test('10% - 2 Children', () => expect(getPaymentAmountForChildren(10, 2, 0)).toBe(0));
-  test('30% - Spouse with AA', () => expect(getRateAmount('aaspouse', 30)).toBe(57.00));
+  test('30% - Spouse with AA', () => expect(getRateAmount('aaspouse', 30, 2024)).toBe(57.00));
 });
 
 describe('2024 Payments', () => {
-  test('Babies Everywhere!', () => expect(calculatePayment(30, { children: 900 })).toBe(28434.31));
-  test('1', () => expect(calculatePayment(30, { isMarried: true })).toBe(586.31));
-  test('2', () => expect(calculatePayment(70, { children: 6 })).toBe(2173.28));
+  test('Babies Everywhere!', () => expect(calculatePayment(30, { children: 900 }, 2024)).toBe(28434.31));
+  test('1', () => expect(calculatePayment(30, { isMarried: true }, 2024)).toBe(586.31));
+  test('2', () => expect(calculatePayment(70, { children: 6 }, 2024)).toBe(2173.28));
 });
 
 export { };
