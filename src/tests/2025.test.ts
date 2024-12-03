2182b182f7d3da07b1150c91a70527726ac7c651
Lee Wise <theleewise@users.noreply.github.com>
2024-12-03 16:14:15 -0600

    correct 2025 rates

--- src/tests/2025.test.ts
+++ src/tests/2025.test.ts
@@ -7,13 +7,13 @@ describe('2025 Rates', () => {
 
 describe('2024 Additional Payments', () => {
   test('10% - 2 Children', () => expect(getPaymentAmountForChildren(10, 2, 0)).toBe(0));
-  test('30% - Spouse with AA', () => expect(getRateAmount('aaspouse', 30, 2025)).toBe(58.43));
+  test('30% - Spouse with AA', () => expect(getRateAmount('aaspouse', 30, 2025)).toBe(58.00));
 });
 
 describe('2025 Payments', () => {
-  test('Babies Everywhere!', () => expect(calculatePayment(30, { children: 900 }, 2025)).toBe(29149.66));
-  test('1', () => expect(calculatePayment(30, { isMarried: true }, 2025)).toBe(600.97));
-  test('2', () => expect(calculatePayment(70, { children: 6 }, 2025)).toBe(2227.61));
+  test('Babies Everywhere!', () => expect(calculatePayment(30, { children: 900 }, 2025)).toBe(28448.42));
+  test('1', () => expect(calculatePayment(30, { isMarried: true }, 2025)).toBe(601.42));
+  test('2', () => expect(calculatePayment(70, { children: 6 }, 2025)).toBe(2228.19));
 });
 
 export { };
